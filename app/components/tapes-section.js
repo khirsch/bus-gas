35a8bc296de6811f688ebd8db7cc8ea9a48c0b0c
Katrina Hirsch <katrina.hirsch@gmail.com>
2017-01-30 20:25:45 -0800

    add shopping cart functionality

--- app/components/tapes-section.js
+++ app/components/tapes-section.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  shoppingCart: Ember.inject.service(),
+  actions: {
+    addToCart(item) {
+      this.sendAction('addToCart', item);
+    },
+    removeQuantity(book) {
+      this.get('shoppingCart').remove(book);
+    },
+    addQuantity(book) {
+      this.get('shoppingCart').add(book);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  shoppingCart: Ember.inject.service(),
   model() {
     return this.store.findAll('tape');
   },
+  actions: {
+    addToCart(item) {
+      this.get('shoppingCart').add(item);
+    }
+  }
 });
